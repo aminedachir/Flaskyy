e627b019358c83b2e028438c18ef718c052b99d6
aminedachir <dachiramine110@gmail.com>
2021-04-13 18:12:38 +0100

    Add a Login Form

--- app1.py
+++ app1.py
@@ -1,4 +1,5 @@
 from flask import Flask , render_template
+from forms import loginForm
 
 app1 = Flask(__name__)
 app1.config.from_object('config')
@@ -12,6 +13,11 @@ def home():
 	{"author":{"username":"Khaled"}},
 	]
 	return render_template('index.html',user=user, title = "Home Page", posts = posts)
+@app1.route('/login')
+def log():
+	form=loginForm()
+	return render_template("login.html", title="Log In", form = form)
+
 
 if __name__ == '__main__':
 	app1.run(port = app1.config["PORT"], debug = app1.config["DEBUG"])
