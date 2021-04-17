b1c060a4a188ca9f0c8333b26917698b9992ab2c
aminedachir <dachiramine110@gmail.com>
2021-04-17 17:13:03 +0100

    DATABASE

--- config.py
+++ config.py
@@ -3,5 +3,6 @@ basedir=os.path.abspath(os.path.dirname(__file__))
 class config(object):
 	SECRET_KEY= "17@lames.com" #os.environ.get("SECRET_KEY") or "17@lames.com"
 	SQLALCHEMY_DATABASE_URI=os.environ.get('DATABASE_URL') or 'sqlite:///'+ os.path.join(basedir,'app.db')
+	SQLALCHEMY_TRACK_MODIFICATIONS = False
 DEBUG = True
 PORT = 1234
