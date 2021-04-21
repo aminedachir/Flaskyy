e789fdc9e5090b08e53c4e4a97b28871410ef560
aminedachir <dachiramine110@gmail.com>
2021-04-21 19:29:24 +0100

    some modifications

--- models.py
+++ models.py
@@ -6,5 +6,3 @@ class user(db.Model):
 	email = db.Column(db.String(120), index=True, unique=True)
 	password_hash = db.Column(db.String(128))
 
-	def __repr__(self):
-		return f"<user {self.username}>"
